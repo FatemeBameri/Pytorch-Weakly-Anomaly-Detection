9af4ae62ae171cc6856fec794aa6f34940afee51
Minoo <24471953+FatemeBameri@users.noreply.github.com>
2024-01-22 18:38:21 +0330

    Update model.py

--- Anomaly Detection/model.py
+++ Anomaly Detection/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+
 torch.set_default_tensor_type('torch.FloatTensor')
 import torch.nn.init as torch_init
 from Cal_pdf import diff_seg, calculating_dist, mean_diff_seg
@@ -14,6 +15,7 @@ def weight_init(m):
         if m.bias is not None:
             m.bias.data.fill_(0)
 
+
 class Model(nn.Module):
     def __init__(self, n_features, batch_size):
         super(Model, self).__init__()
@@ -22,7 +24,7 @@ class Model(nn.Module):
         self.k_abn = self.num_segments // 10
         self.k_nor = self.num_segments // 10
 
-        self.Atten = RTFM_Attention(len_feature=1024)
+        self.Atten = RTFM_Attention(len_feature=2048)
 
         self.fc1 = nn.Linear(n_features, 512)
         self.fc2 = nn.Linear(512, 128)
@@ -41,9 +43,9 @@ class Model(nn.Module):
         bs, ncrops, t, f = out.size()
 
         out = out.view(-1, t, f)
-      
+
         out = self.Atten(out)
-     
+
         out = self.drop_out(out)
 
         features = self.relu(out)
@@ -55,42 +57,42 @@ class Model(nn.Module):
         scores = self.sigmoid(self.fc3(scores))
         scores = scores.view(bs, ncrops, -1).mean(1)
         scores = scores.unsqueeze(dim=2)
-        normal_features = features[0:self.batch_size*10]
+        normal_features = features[0:self.batch_size * 10]
         normal_scores = scores[0:self.batch_size]
-        abnormal_features = features[self.batch_size*10:]
+        abnormal_features = features[self.batch_size * 10:]
         abnormal_scores = scores[self.batch_size:]
 
-       ###########################################################
+        ###########################################################
 
         if bs == 1:
             abnormal_features = normal_features
             abnormal_scores = normal_scores
             n_size = bs
         else:
-            n_size=int(bs/2)
+            n_size = int(bs / 2)
 
-    ############## Estimation Distribution for videos###############
+        ############## Estimation Distribution for videos###############
 
-
-        abnormal_features2 = abnormal_features.view(n_size,ncrops,t,f).mean(1)
-        normal_features2 = normal_features.view(n_size,ncrops,t,f).mean(1)
+        abnormal_features2 = abnormal_features.view(n_size, ncrops, t, f).mean(1)
+        normal_features2 = normal_features.view(n_size, ncrops, t, f).mean(1)
         type_dist = ['param', 'non-param']
-        test_type=type_dist [0]
+        test_type = type_dist[0]
 
         # (0,2)-(2,0)-(1,4)-(4,1)-(5,6)-(6,5)-(8,2)-(2,8)-(7,4)-(4,7)-kde
-        index_dist= 4
-        index_distn = 7
-        abnormal_dist_features = calculating_dist(test_type, abnormal_features2, index_dist)
-        normal_dist_features = calculating_dist(test_type, normal_features2, index_distn)
-        #--------------------------------------------------------------------------------------
-        topk=4
+        index_dist = 6
+        index_distn = 5
+        abnormal_dist_features = calculating_dist(test_type, abnormal_features, index_dist)
+        normal_dist_features = calculating_dist(test_type, normal_features, index_distn)
+
+        # --------------------------------------------------------------------------------------
+        topk = 4
         topk_ab = torch.topk(abnormal_dist_features, topk)
         topk_n = torch.topk(normal_dist_features, topk)
 
         ######################### Comparing Abnormal Distributions #############
 
-        k_near = 3  # 3,4,5,6,7,8,9
-        total_diff_abnormal,total_diff_index_q = diff_seg(n_size,t, abnormal_features2, k_near)
+        k_near = 3  # 1,2,3,4,5,6,7,8,9
+        total_diff_abnormal, total_diff_index_q = diff_seg(n_size, t, abnormal_features2, k_near)
         ############################################################################################
         total_diff_abnormal = torch.tensor(total_diff_abnormal)
         select_idx = torch.ones_like(total_diff_abnormal)
@@ -105,9 +107,10 @@ class Model(nn.Module):
 
         idx_abn_feat = total_new_idx_abn.unsqueeze(2).expand([-1, -1, f])
 
-        abnormal_features = abnormal_features.view(n_size, ncrops, t, f)
+        abnormal_features = abnormal_dist_features.view(n_size, ncrops, t, f)
+        #abnormal_features = abnormal_features.view(n_size, ncrops, t, f)
         abnormal_features = abnormal_features.permute(1, 0, 2, 3)
-        #idx_abn_feat = idx_abn_feat.to('cuda')
+        # idx_abn_feat = idx_abn_feat.to('cuda')
         idx_abn_feat = idx_abn_feat.to('cpu')
         total_select_abn_feature = torch.zeros(0, device=inputs.device)
         for abnormal_feature in abnormal_features:
@@ -116,10 +119,10 @@ class Model(nn.Module):
             total_select_abn_feature = torch.cat((total_select_abn_feature, feat_select_abn))
 
         idx_abn_score = total_new_idx_abn.unsqueeze(2).expand([-1, -1, abnormal_scores.shape[2]])
-        #abnormal_scores = abnormal_scores.to('cuda')
-        #idx_abn_score = idx_abn_score.to('cuda')
-        abnormal_scores=abnormal_scores.to('cpu')
-        idx_abn_score=idx_abn_score.to('cpu')
+        # abnormal_scores = abnormal_scores.to('cuda')
+        # idx_abn_score = idx_abn_score.to('cuda')
+        abnormal_scores = abnormal_scores.to('cpu')
+        idx_abn_score = idx_abn_score.to('cpu')
         score_abnormal = torch.mean(torch.gather(abnormal_scores, 1, idx_abn_score),
                                     dim=1)  # top 3 scores in abnormal bag
 
@@ -139,12 +142,12 @@ class Model(nn.Module):
 
         total_new_idx_n = correct_index(n_size, idx_n, diff_index_nq)
 
-
         idx_n_feat = total_new_idx_n.unsqueeze(2).expand([-1, -1, f])
 
-        normal_features = normal_features.view(n_size, ncrops, t, f)
+        normal_features = normal_dist_features.view(n_size, ncrops, t, f)
+        #normal_features = normal_features.view(n_size, ncrops, t, f)
         normal_features = normal_features.permute(1, 0, 2, 3)
-        #idx_n_feat = idx_n_feat.to('cuda')
+        # idx_n_feat = idx_n_feat.to('cuda')
         idx_n_feat = idx_n_feat.to('cpu')
         total_select_n_feature = torch.zeros(0, device=inputs.device)
         for normal_feature in normal_features:
@@ -153,19 +156,19 @@ class Model(nn.Module):
             total_select_n_feature = torch.cat((total_select_n_feature, feat_select_n))
 
         idx_n_score = total_new_idx_n.unsqueeze(2).expand([-1, -1, normal_scores.shape[2]])
-        #normal_scores = normal_scores.to('cuda')
-        #idx_n_score = idx_n_score.to('cuda')
-        normal_scores=normal_scores.to('cpu')
-        idx_n_score=idx_n_score.to('cpu')
+        # normal_scores = normal_scores.to('cuda')
+        # idx_n_score = idx_n_score.to('cuda')
+        normal_scores = normal_scores.to('cpu')
+        idx_n_score = idx_n_score.to('cpu')
         score_normal = torch.mean(torch.gather(normal_scores, 1, idx_n_score),
                                   dim=1)  # top 3 scores in normal bag
 
-        feat_abnormal =total_select_abn_feature
+        feat_abnormal = total_select_abn_feature
         feat_normal = total_select_n_feature
         feat_dist_abnormal = topk_ab.values
         feat_dist_normal = topk_n.values
 
         scores = torch.cat((normal_scores, abnormal_scores), 0)
 
-        return score_abnormal, score_normal, feat_dist_abnormal, feat_dist_normal,\
-        feat_abnormal, feat_normal, scores
+        return score_abnormal, score_normal, feat_dist_abnormal, feat_dist_normal, \
+            feat_abnormal, feat_normal, scores
